87914d919df9f23504887dcc23fcba1aeaf3bc58
JeffVince <jeff@ophelia.company>
2022-06-17 10:21:36 -0700

    10 binaries uploaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,29 @@ export class AppComponent  {
 
   Mousex: number = 0; 
   MouseY: number = 0;
-  imageSrc: string = '../assets/image/eye-state-00000.png';
+  photo_id: number = 0;
+
+  imageSrc: string = 'https://raw.githubusercontent.com/JeffVince/angular-ivy-jkpus9/master/src/assets/images/eye_state_00000.png';
+
+  imageSrcFront: string = "https://raw.githubusercontent.com/JeffVince/angular-ivy-jkpus9/master/src/assets/images/eye_state_000";
+
+  imageSrcEnd: string = ".png"
+
+  showEyeTracking: boolean = true;
 
   mouseMoved(event: MouseEvent) {
-    this.Mousex = event.clientX;
-    this.MouseY = event.clientY;
-
-    console.log(Math.trunc(63.6618 * Math.atan2(this.MouseY, this.Mousex)));
-
-    
-
+    if (this.showEyeTracking) {
+      this.Mousex = event.clientX;
+      this.MouseY = event.clientY;
+      this.photo_id = 100-Math.trunc(63.6618 * Math.atan2(this.MouseY, this.Mousex));
+      this.imageSrc = this.imageSrcFront + String(this.photo_id) + this.imageSrcEnd;
+    }
   }
+
+  mouseClicked(event: MouseEvent) {
+    this.showEyeTracking = !this.showEyeTracking;
+    console.log("test");
+  }
+
+
 }
